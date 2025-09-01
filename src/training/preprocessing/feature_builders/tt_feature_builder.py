d21389965fc2f93211ec0a7c0fbf71bac33ba935
Salvatore <bonaccos@ee.ethz.ch>
2025-09-01 10:28:19 +0800

    .

--- src/training/preprocessing/feature_builders/tt_feature_builder.py
+++ src/training/preprocessing/feature_builders/tt_feature_builder.py
@@ -9,6 +9,8 @@ from nuplan.common.actor_state.tracked_objects import TrackedObjects
 from nuplan.common.maps.abstract_map import AbstractMap, PolygonMapObject
 from nuplan.common.actor_state.state_representation import Point2D, StateSE2
 from nuplan.planning.scenario_builder.abstract_scenario import AbstractScenario
+from nuplan.common.actor_state.tracked_objects_types import TrackedObjectType
+from nuplan.common.actor_state.vehicle_parameters import get_pacifica_parameters
 from nuplan.planning.simulation.planner.abstract_planner import (
     PlannerInitialization,
     PlannerInput,
@@ -27,6 +29,9 @@ from nuplan.planning.training.preprocessing.features.abstract_model_feature impo
 from shapely import LineString, Point
 
 from src.training.preprocessing.features.tt_feature import TTFeature
+from src.training.scenario_manager.scenario_manager import OccupancyType, ScenarioManager
+from src.training.scenario_manager.utils.route_utils import get_current_roadblock_candidates
+from . import common
 
 
 class TTFeatureBuilder(AbstractFeatureBuilder):
@@ -34,6 +39,8 @@ class TTFeatureBuilder(AbstractFeatureBuilder):
         self,
         history_horizon: float = 2,
         future_horizon: float = 8,
+        max_agents: int = 64,
+        disable_agent: bool = False,
 
         radius: float = 100,
         sample_interval: float = 0.1,
@@ -46,6 +53,31 @@ class TTFeatureBuilder(AbstractFeatureBuilder):
         self.radius = radius
         self.history_samples = int(self.history_horizon / sample_interval)
         self.future_samples = int(self.future_horizon / sample_interval)
+        self.ego_params = get_pacifica_parameters()
+        self.length = self.ego_params.length
+        self.width = self.ego_params.width
+        self.scenario_manager = None
+        self.simulation = False
+        self.max_agents = max_agents
+        self.disable_agent = disable_agent
+        self.interested_objects_types = [
+            TrackedObjectType.EGO,
+            TrackedObjectType.VEHICLE,
+            TrackedObjectType.PEDESTRIAN,
+            TrackedObjectType.BICYCLE,
+        ]
+        self.static_objects_types = [
+            TrackedObjectType.CZONE_SIGN,
+            TrackedObjectType.BARRIER,
+            TrackedObjectType.TRAFFIC_CONE,
+            TrackedObjectType.GENERIC_OBJECT,
+        ]
+
+        self.polygon_types = [
+            SemanticMapLayer.LANE,
+            SemanticMapLayer.LANE_CONNECTOR,
+            SemanticMapLayer.CROSSWALK,
+        ]
 
     def get_feature_type(self) -> Type[AbstractModelFeature]:
         """Inherited, see superclass."""
@@ -140,8 +172,408 @@ class TTFeatureBuilder(AbstractFeatureBuilder):
         query_xy = present_ego_state.center
         traffic_light_status = list(traffic_light_status)  # note: tl is a iterator
         if self.scenario_manager is None:
-            pass
+            scenario_manager = ScenarioManager(
+                map_api,
+                present_ego_state,
+                route_roadblocks_ids,
+                radius=50,
+            )
+            scenario_manager.update_ego_state(present_ego_state)
+            scenario_manager.update_drivable_area_map()
         else:
+            scenario_manager = self.scenario_manager
+        route_roadblocks_ids = scenario_manager.get_route_roadblock_ids()
+        route_reference_path = scenario_manager.update_ego_path()
+
+        scenario_manager.update_obstacle_map(
+            tracked_objects_list[present_idx], traffic_light_status
+        )
+        ego_lanes, ego_blocks = scenario_manager.get_ego_lane_id(ego_state_list)
+        data = {}
+        data["current_state"] = self._get_ego_current_state(
+            ego_state_list[present_idx], ego_state_list[present_idx - 1]
+        )
+        ego_features = self._get_ego_features(ego_states=ego_state_list)
+        # self.get_ego_lane_id(ego_states=ego_state_list, map_api=map_api)
+        agent_features, agent_tokens, agents_polygon = self._get_agent_features(
+            query_xy=query_xy,
+            present_idx=present_idx,
+            tracked_objects_list=tracked_objects_list,
+        )
+        data["agent"] = {}
+        for k in agent_features.keys():
+            data["agent"][k] = np.concatenate(
+                [ego_features[k][None, ...], agent_features[k]], axis=0
+            )
+        agent_tokens = ["ego"] + agent_tokens
+
+        data["static_objects"] = self._get_static_objects_features(
+            present_ego_state, scenario_manager, tracked_objects_list[present_idx]
+        )
+
+        data["map"], map_polygon_tokens = self._get_map_features(
+            map_api=map_api,
+            query_xy=query_xy,
+            route_roadblock_ids=route_roadblocks_ids,
+            traffic_light_status=traffic_light_status,
+            radius=self.radius,
+        )
+
+        return TTFeature.normalize(data, first_time=True, radius=self.radius)
+
+    def _get_ego_current_state(self, ego_state: EgoState, prev_state: EgoState):
+        state = np.zeros(7, dtype=np.float64)
+        state[0:2] = ego_state.rear_axle.array
+        state[2] = ego_state.rear_axle.heading
+        state[3] = ego_state.dynamic_car_state.rear_axle_velocity_2d.x
+        state[4] = ego_state.dynamic_car_state.rear_axle_acceleration_2d.x
+
+        if self.simulation:
+            steering_angle, yaw_rate = (
+                ego_state.tire_steering_angle,
+                ego_state.dynamic_car_state.angular_velocity,
+            )
+        else:
+            steering_angle, yaw_rate = self.calculate_additional_ego_states(
+                ego_state, prev_state
+            )
+
+        state[5] = steering_angle
+        state[6] = yaw_rate
+
+        return state
+
+    def _get_ego_features(self, ego_states: List[EgoState]):
+        """note that rear axle velocity and acceleration are in ego local frame,
+        and need to be transformed to the global frame.
+        """
+        T = len(ego_states)
+        position = np.zeros((T, 2), dtype=np.float64)
+        heading = np.zeros((T), dtype=np.float64)
+        velocity = np.zeros((T, 2), dtype=np.float64)
+        acceleration = np.zeros((T, 2), dtype=np.float64)
+        shape = np.zeros((T, 2), dtype=np.float64)
+        valid_mask = np.ones(T, dtype=np.bool_)
+
+        for t, state in enumerate(ego_states):
+            position[t] = state.rear_axle.array
+            heading[t] = state.rear_axle.heading
+            velocity[t] = common.rotate_round_z_axis(
+                state.dynamic_car_state.rear_axle_velocity_2d.array,
+                -state.rear_axle.heading,
+            )
+            acceleration[t] = common.rotate_round_z_axis(
+                state.dynamic_car_state.rear_axle_acceleration_2d.array,
+                -state.rear_axle.heading,
+            )
+            shape[t] = np.array([self.width, self.length])
+
+        category = np.array(
+            self.interested_objects_types.index(TrackedObjectType.EGO), dtype=np.int8
+        )
+
+        return {
+            "position": position,
+            "heading": heading,
+            "velocity": velocity,
+            "acceleration": acceleration,
+            "shape": shape,
+            "category": category,
+            "valid_mask": valid_mask,
+        }
+
+    def _get_agent_features(
+        self,
+        query_xy: Point2D,
+        present_idx: int,
+        tracked_objects_list: List[TrackedObjects],
+    ):
+        present_tracked_objects = tracked_objects_list[present_idx]
+        present_agents = present_tracked_objects.get_tracked_objects_of_types(
+            self.interested_objects_types
+        )
+        N, T = min(len(present_agents), self.max_agents), len(tracked_objects_list)
+
+        position = np.zeros((N, T, 2), dtype=np.float64)
+        heading = np.zeros((N, T), dtype=np.float64)
+        velocity = np.zeros((N, T, 2), dtype=np.float64)
+        shape = np.zeros((N, T, 2), dtype=np.float64)
+        category = np.zeros((N,), dtype=np.int8)
+        valid_mask = np.zeros((N, T), dtype=np.bool_)
+        polygon = [None] * N
+
+        if N == 0 or self.disable_agent:
+            return (
+                {
+                    "position": position,
+                    "heading": heading,
+                    "velocity": velocity,
+                    "shape": shape,
+                    "category": category,
+                    "valid_mask": valid_mask,
+                },
+                [],
+                [],
+            )
+
+        agent_ids = np.array([agent.track_token for agent in present_agents])
+        agent_cur_pos = np.array([agent.center.array for agent in present_agents])
+        distance = np.linalg.norm(agent_cur_pos - query_xy.array[None, :], axis=1)
+        agent_ids_sorted = agent_ids[np.argsort(distance)[: self.max_agents]]
+        agent_ids_dict = {agent_id: i for i, agent_id in enumerate(agent_ids_sorted)}
+
+        for t, tracked_objects in enumerate(tracked_objects_list):
+            for agent in tracked_objects.get_tracked_objects_of_types(
+                self.interested_objects_types
+            ):
+                if agent.track_token not in agent_ids_dict:
+                    continue
+
+                idx = agent_ids_dict[agent.track_token]
+                position[idx, t] = agent.center.array
+                heading[idx, t] = agent.center.heading
+                velocity[idx, t] = agent.velocity.array
+                shape[idx, t] = np.array([agent.box.width, agent.box.length])
+                valid_mask[idx, t] = True
+
+                if t == present_idx:
+                    category[idx] = self.interested_objects_types.index(
+                        agent.tracked_object_type
+                    )
+                    polygon[idx] = agent.box.geometry
+
+        agent_features = {
+            "position": position,
+            "heading": heading,
+            "velocity": velocity,
+            "shape": shape,
+            "category": category,
+            "valid_mask": valid_mask,
+        }
+
+        return agent_features, list(agent_ids_sorted), polygon
+
+    def _get_static_objects_features(
+        self,
+        ego_state: EgoState,
+        scenario_manager: ScenarioManager,
+        tracked_objects_list: TrackedObjects,
+    ):
+        static_objects = []
+
+        # only cares objects that are in drivable area
+        for obj in tracked_objects_list.get_static_objects():
+            if np.linalg.norm(ego_state.center.array - obj.center.array) > self.radius:
+                continue
+            if not scenario_manager.object_in_drivable_area(obj.box.geometry):
+                continue
+            static_objects.append(
+                np.concatenate(
+                    [
+                        obj.center.array,
+                        [obj.center.heading],
+                        [obj.box.width, obj.box.length],
+                        [self.static_objects_types.index(obj.tracked_object_type)],
+                    ],
+                    axis=-1,
+                    dtype=np.float64,
+                )
+            )
+
+        if len(static_objects) > 0:
+            static_objects = np.stack(static_objects, axis=0)
+            valid_mask = np.ones(len(static_objects), dtype=np.bool_)
+        else:
+            static_objects = np.zeros((0, 6), dtype=np.float64)
+            valid_mask = np.zeros(0, dtype=np.bool_)
+
+        return {
+            "position": static_objects[:, :2],
+            "heading": static_objects[:, 2],
+            "shape": static_objects[:, 3:5],
+            "category": static_objects[:, -1],
+            "valid_mask": valid_mask,
+        }
+
+    def _get_map_features(
+        self,
+        map_api: AbstractMap,
+        query_xy: Point2D,
+        route_roadblock_ids: List[str],
+        traffic_light_status: List[TrafficLightStatusData],
+        radius: float,
+        sample_points: int = 20,
+    ):
+        route_ids = set(int(route_id) for route_id in route_roadblock_ids)
+        tls = {tl.lane_connector_id: tl.status for tl in traffic_light_status}
+
+        map_objects = map_api.get_proximal_map_objects(
+            query_xy,
+            radius,
+            [
+                SemanticMapLayer.LANE,
+                SemanticMapLayer.LANE_CONNECTOR,
+                SemanticMapLayer.CROSSWALK,
+            ],
+        )
+        lane_objects = (
+            map_objects[SemanticMapLayer.LANE]
+            + map_objects[SemanticMapLayer.LANE_CONNECTOR]
+        )
+        crosswalk_objects = map_objects[SemanticMapLayer.CROSSWALK]
+
+        object_ids = [int(obj.id) for obj in lane_objects + crosswalk_objects]
+        object_types = (
+            [SemanticMapLayer.LANE] * len(map_objects[SemanticMapLayer.LANE])
+            + [SemanticMapLayer.LANE_CONNECTOR]
+            * len(map_objects[SemanticMapLayer.LANE_CONNECTOR])
+            + [SemanticMapLayer.CROSSWALK]
+            * len(map_objects[SemanticMapLayer.CROSSWALK])
+        )
+
+        M, P = len(lane_objects) + len(crosswalk_objects), sample_points
+        point_position = np.zeros((M, 3, P, 2), dtype=np.float64)
+        point_vector = np.zeros((M, 3, P, 2), dtype=np.float64)
+        point_side = np.zeros((M, 3), dtype=np.int8)
+        point_orientation = np.zeros((M, 3, P), dtype=np.float64)
+        polygon_center = np.zeros((M, 3), dtype=np.float64)
+        polygon_position = np.zeros((M, 2), dtype=np.float64)
+        polygon_orientation = np.zeros(M, dtype=np.float64)
+        polygon_type = np.zeros(M, dtype=np.int8)
+        polygon_on_route = np.zeros(M, dtype=np.bool_)
+        polygon_tl_status = np.zeros(M, dtype=np.int8)
+        polygon_speed_limit = np.zeros(M, dtype=np.float64)
+        polygon_has_speed_limit = np.zeros(M, dtype=np.bool_)
+        polygon_road_block_id = np.zeros(M, dtype=np.int32)
+
+        for lane in lane_objects:
+            object_id = int(lane.id)
+            idx = object_ids.index(object_id)
+            speed_limit = lane.speed_limit_mps
+
+            centerline = self._sample_discrete_path(
+                lane.baseline_path.discrete_path, sample_points + 1
+            )
+            left_bound = self._sample_discrete_path(
+                lane.left_boundary.discrete_path, sample_points + 1
+            )
+            right_bound = self._sample_discrete_path(
+                lane.right_boundary.discrete_path, sample_points + 1
+            )
+            edges = np.stack([centerline, left_bound, right_bound], axis=0)
+
+            point_vector[idx] = edges[:, 1:] - edges[:, :-1]
+            point_position[idx] = edges[:, :-1]
+            point_orientation[idx] = np.arctan2(
+                point_vector[idx, :, :, 1], point_vector[idx, :, :, 0]
+            )
+            point_side[idx] = np.arange(3)
+
+            polygon_center[idx] = np.concatenate(
+                [
+                    centerline[int(sample_points / 2)],
+                    [point_orientation[idx, 0, int(sample_points / 2)]],
+                ],
+                axis=-1,
+            )
+            polygon_position[idx] = centerline[0]
+            polygon_orientation[idx] = point_orientation[idx, 0, 0]
+            polygon_type[idx] = self.polygon_types.index(object_types[idx])
+            polygon_on_route[idx] = int(lane.get_roadblock_id()) in route_ids
+            polygon_tl_status[idx] = (
+                tls[object_id] if object_id in tls else TrafficLightStatusType.UNKNOWN
+            )
+            polygon_has_speed_limit[idx] = speed_limit is not None
+            polygon_speed_limit[idx] = (
+                lane.speed_limit_mps if lane.speed_limit_mps else 0
+            )
+            polygon_road_block_id[idx] = int(lane.get_roadblock_id())
+
+        for crosswalk in crosswalk_objects:
+            idx = object_ids.index(int(crosswalk.id))
+            edges = self._get_crosswalk_edges(crosswalk)
+            point_vector[idx] = edges[:, 1:] - edges[:, :-1]
+            point_position[idx] = edges[:, :-1]
+            point_orientation[idx] = np.arctan2(
+                point_vector[idx, :, :, 1], point_vector[idx, :, :, 0]
+            )
+            point_side[idx] = np.arange(3)
+            polygon_center[idx] = np.concatenate(
+                [
+                    edges[0, int(sample_points / 2)],
+                    [point_orientation[idx, 0, int(sample_points / 2)]],
+                ],
+                axis=-1,
+            )
+            polygon_position[idx] = edges[0, 0]
+            polygon_orientation[idx] = point_orientation[idx, 0, 0]
+            polygon_type[idx] = self.polygon_types.index(object_types[idx])
+            polygon_on_route[idx] = False
+            polygon_tl_status[idx] = TrafficLightStatusType.UNKNOWN
+            polygon_has_speed_limit[idx] = False
+
+        map_features = {
+            "point_position": point_position,
+            "point_vector": point_vector,
+            "point_orientation": point_orientation,
+            "point_side": point_side,
+            "polygon_center": polygon_center,
+            "polygon_position": polygon_position,
+            "polygon_orientation": polygon_orientation,
+            "polygon_type": polygon_type,
+            "polygon_on_route": polygon_on_route,
+            "polygon_tl_status": polygon_tl_status,
+            "polygon_has_speed_limit": polygon_has_speed_limit,
+            "polygon_speed_limit": polygon_speed_limit,
+            "polygon_road_block_id": polygon_road_block_id,
+        }
+
+        return map_features, object_ids
+
+    def _sample_discrete_path(self, discrete_path: List[StateSE2], num_points: int):
+        path = np.stack([point.array for point in discrete_path], axis=0)
+        return common.interpolate_polyline(path, num_points)
+
+    def calculate_additional_ego_states(
+        self, current_state: EgoState, prev_state: EgoState, dt=0.1
+    ):
+        cur_velocity = current_state.dynamic_car_state.rear_axle_velocity_2d.x
+        angle_diff = current_state.rear_axle.heading - prev_state.rear_axle.heading
+        angle_diff = (angle_diff + np.pi) % (2 * np.pi) - np.pi
+        yaw_rate = angle_diff / dt
+
+        if abs(cur_velocity) < 0.2:
+            return 0.0, 0.0  # if the car is almost stopped, the yaw rate is unreliable
+        else:
+            steering_angle = np.arctan(
+                yaw_rate * self.ego_params.wheel_base / abs(cur_velocity)
+            )
+            steering_angle = np.clip(steering_angle, -2 / 3 * np.pi, 2 / 3 * np.pi)
+            yaw_rate = np.clip(yaw_rate, -0.95, 0.95)
+
+            return steering_angle, yaw_rate
+
+    def get_ego_lane_id(self, ego_states: List[EgoState], map_api):
+        route_lane_dict = self.scenario_manager.get_route_lane_dicts()
+        route_roadblock_ids = self.scenario_manager.get_route_roadblock_ids()
+        for ego_state in ego_states:
+            current_block, current_block_candidates = (
+                get_current_roadblock_candidates(ego_state, map_api, route_roadblock_ids))
+            lanes = current_block.interior_edges
+            # tmp_lanes = self.scenario_manager._route_manager.
             pass
 
-        return 1
+    def _get_crosswalk_edges(
+        self, crosswalk: PolygonMapObject, sample_points: int = 21
+    ):
+        bbox = shapely.minimum_rotated_rectangle(crosswalk.polygon)
+        coords = np.stack(bbox.exterior.coords.xy, axis=-1)
+        edge1 = coords[[3, 0]]  # right boundary
+        edge2 = coords[[2, 1]]  # left boundary
+
+        edges = np.stack([(edge1 + edge2) * 0.5, edge2, edge1], axis=0)  # [3, 2, 2]
+        vector = edges[:, 1] - edges[:, 0]  # [3, 2]
+        steps = np.linspace(0, 1, sample_points, endpoint=True)[None, :]
+        points = edges[:, 0][:, None, :] + vector[:, None, :] * steps[:, :, None]
+
+        return points
